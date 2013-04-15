3f5ef08d66dd8a06d6605047b202baddc536b9ae
jafeha <jafeha@zankapfel.org>
2013-04-15 14:50:43 +0200

    update_config() function added

--- parabird_build_argparse.py
+++ parabird_build_argparse.py
@@ -36,51 +36,77 @@ print "[INFO] Checking all Dependencies..."
 FNULL = open(os.devnull, 'w')
 
 try:
-	
 	subprocess.check_call(["truecrypt", "--version"], stdout=FNULL)
+
 except OSError: 
 	print "[ERROR] Missing Depedencies: Truecrypt not installed"
+
 try:
 	subprocess.check_call(["7z"], stdout=FNULL)
+
 except OSError:
 	print "[ERROR] Missing Depedencies: 7zip not installed"
 
 print "[INFO] Configuring..."
 
+def update_config(section, key, value_from_argparser):
+        
+	if value_from_argparser:
+		print "[INFO] Parameter given, device is:", value_from_argparser
+		parser.set(section, key, value_from_argparser)
+
+	if value_from_argparser == None:
+		print "[INFO] Setting", value_from_argparser, "to Parameter from Config File:", parser.get(section, key)
+
+
+
 try:
-	if args.device:
-		print "[INFO] Parameter given, device is:", args.device
+
+update_config(DEFAULT, device, args.device)
+update_config(thunderbird, version, args.thunder)
+update_config(torbirdy, version, args.torbirdy)
+update_config(enigmail, version, args.enigmail)
+update_config(vidalia, version, args.vidalia)
+
+
+#	if args.device:
+#		print "[INFO] Parameter given, device is:", args.device
+#		parser.set('DEFAULT', 'device', args.device)
+
+#	if args.device == None:
+#		print "[INFO] Setting Device Parameter from Config File:"
+#		args.device = parser.get('DEFAULT', 'device')
+#		print "[CONFIG] Parameter is set to", args.device
 	
-	if args.device == None:
-		print "[INFO] Setting Device Parameter from Config File:"
-		args.device = parser.get('DEFAULT', 'device')
-		print "[CONFIG] Parameter is set to", args.device
-
-	if args.thunder == None:
-		print "[INFO] Setting Thunderbird Version Parameter from Config File:"
-		args.thunder = parser.get('thunderbird', 'version')
-		print "[CONFIG] Parameter is set to", args.thunder
-
-	if args.torbirdy == None:
-		print "[INFO] Setting Torbirdy Version Parameter from Config File:"
-		args.torbirdy = parser.get('torbirdy', 'version')
-		print "[CONFIG] Parameter is set to", args.torbirdy
-
-	if args.enigmail == None:
-		print "[INFO] Setting Torbirdy Version Parameter from Config File:"
-		args.enigmail = parser.get('enigmail', 'version')
-		print "[CONFIG] Parameter is set to", args.enigmail
-
-	if args.vidalia == None:
-		print "[INFO] Setting Vidalia Version Parameter from Config File:"
-		args.vidalia = parser.get('vidalia', 'version')
-		print "[CONFIG] Parameter is set to", args.vidalia
-
+#	if args.thunder:
+#		print "[INFO] Parameter given, Thunderbird Version is:", args.thunder
+#		parser.set('thunderbird', 'version', args.thunder)
+#
+#	if args.thunder == None:
+#		print "[INFO] Setting Thunderbird Version Parameter from Config File:"
+#		args.thunder = parser.get('thunderbird', 'version')
+#		print "[CONFIG] Parameter is set to", args.thunder
+#
+#	if args.torbirdy == None:
+#		print "[INFO] Setting Torbirdy Version Parameter from Config File:"
+#		args.torbirdy = parser.get('torbirdy', 'version')
+#		print "[CONFIG] Parameter is set to", args.torbirdy
+#
+#	if args.enigmail == None:
+#		print "[INFO] Setting Torbirdy Version Parameter from Config File:"
+#		args.enigmail = parser.get('enigmail', 'version')
+#		print "[CONFIG] Parameter is set to", args.enigmail
+#
+#	if args.vidalia == None:
+#		print "[INFO] Setting Vidalia Version Parameter from Config File:"
+#		args.vidalia = parser.get('vidalia', 'version')
+#		print "[CONFIG] Parameter is set to", args.vidalia
+#
 except NameError: 
 #  args.device = parser.get('device')
 
-  print "Parameter taken from Config file, device is:", parser.get('device')
-  print "Hier ist was ganz arg schiefgelaufen"
+	print "Parameter taken from Config file, device is:", parser.get('device')
+	print "Hier ist was ganz arg schiefgelaufen"
 
 
 print "[INFO] Creating Mountpoint"
@@ -130,6 +156,31 @@ except:
 
 
 print "[INFO] Configure Extensions and Profile Folder"
+
+print "[INFO] Downloading Vidalia [Linux]"
+
+try:
+        urllib.urlretreive(parser.get('vidalia',  'linux_url'), filename="/tmp/thunderbird_mac.zip")
+except:
+	print "Could not Download Vidalia for Linux"
+
+
+print "[INFO] Downloading Vidalia [Windows]"
+
+try:
+        urllib.urlretreive(parser.get('vidalia',  'windows_url'), filename="/tmp/thunderbird_mac.zip")
+except:
+        print "Could not Download Vidalia for Windows"
+
+
+print "[INFO] Downloading Vidalia [Mac OS]"
+
+try:
+        urllib.urlretreive(parser.get('vidalia',  'mac_url'), filename="/tmp/thunderbird_mac.zip")
+except:
+        print "Could not Download Vidalia for Mac OS"
+
+
 print "[INFO] Unmounting Truecrypt Container"
 print "[INFO] Unmounting USB-Stick"
 
