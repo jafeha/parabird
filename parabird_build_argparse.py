de2015e4bf5bde659e971678ba36648e3f86d96b
Jakob Hasselmann <jafeha@box.(none)>
2013-04-16 18:47:35 +0200

    Several Improvements and Bugfixes

--- parabird_build_argparse.py
+++ parabird_build_argparse.py
@@ -8,10 +8,11 @@ import subprocess
 import sys
 import os
 import tempfile
+import shlex
 
 mountpoint = tempfile.mkdtemp()
 tempdir = tempfile.mkdtemp()
-
+tc_mountpoint = tempfile.mkdtemp()
 
 def dependency_check(checked_app):
 
@@ -52,14 +53,9 @@ parser.add_argument("-t", "--thunder", help="Specify Thunderbird version to down
 parser.add_argument("-b", "--torbirdy", help="Specify Torbirdy Version")
 parser.add_argument("-e", "--enigmail", help="Specify Enigmail Version") 
 parser.add_argument("-a", "--vidalia", help="Specify Vidalia Version")
+parser.add_argument("-n", "--container_name", help="Specify Container Name")
 
 args = parser.parse_args()
-
-if args.verbose:
-   print "verbosity turned on"
-
-if args.device:
-   print "Device is:", args.device
 
 from ConfigParser import SafeConfigParser
 
@@ -67,15 +63,21 @@ parser = SafeConfigParser()
 with codecs.open('config.ini', 'r', encoding='utf-8') as f:
     parser.readfp(f)
 
+# Removed, because there is no verbosity support, could be reimplemented later.
+#if args.verbose:
+#   print "verbosity turned on"
 
 print "[INFO] Checking all Dependencies..."
 
-
-dependency_check(["truecrypt", "--text", "--version"])
-dependency_check("7z")
+try:
+	dependency_check(["truecrypt", "--text", "--version"])
+	dependency_check("7z")
+except: 
+	print "[ERROR] Dependency Checks failed large scale, exiting..."
+	from sys import exit
+	exit()
 
 print "[INFO] Configuring..."
-
 
 try:
 	update_config("DEFAULT", "device", args.device)
@@ -83,6 +85,7 @@ try:
 	update_config("torbirdy", "version", args.torbirdy)
 	update_config("enigmail", "version", args.enigmail)
 	update_config("vidalia", "version", args.vidalia)
+	update_config("DEFAULT", "container_name", args.container_name)
 
 except NameError: 
 	print "[ERROR] Hier ist was ganz arg schiefgelaufen"
@@ -95,27 +98,50 @@ except:
 	print "[ERROR] Mounting", parser.get('DEFAULT', 'device'), "to", mountpoint, "failed"
 
 print "[INFO] Creating Truecrypt Container on USB-Stick"
+
+# Something goes wrong here. Container seems to get created but i won't find it on the USB Stick
+# Note: this should actually work by now. But there seems further testing nessesary.
+ 
+container_path = mountpoint+"/"+parser.get('DEFAULT', 'container_name')
+print "[INFO] Creating", container_path
+
+tc = parser.get('truecrypting', 'create')
+tc_create = shlex.split(tc)
+subprocess.check_call(tc_create)
+
 print "[INFO] Mounting Truecrypt Container"
+
+tc = parser.get('truecrypting', 'mount')
+tc_mount = shlex.split(tc)
+subprocess.check_call(tc_mount)
+
 print "[INFO] Creating Folders in Truecrypt Container"
 
-print "[INFO] Starting to download Applications to", tempdir
+print "[INFO] Starting to download Applications to:", tempdir
+
+# Disabled all downloads except of one, there is no need to download everything every single
+# time i run this script. Re-enable them step by step.
 
 download_application("Thunderbird [Linux]", parser.get('thunderbird', 'linux_url'))
-download_application("Thunderbird [Windows]", parser.get('thunderbird', 'windows_url'))
-download_application("Thunderbird [Mac OS]", parser.get('thunderbird', 'mac_url'))
-download_application("Torbirdy", parser.get('torbirdy', 'url'))
-download_application("Enigmail", parser.get('enigmail', 'url'))
-download_application("Vidalia [Linux]", parser.get('vidalia', 'linux_url'))
-download_application("Vidalia [Windows]", parser.get('vidalia', 'windows_url'))
-download_application("Vidalia [Mac OS]", parser.get('vidalia', 'mac_url'))
+#download_application("Thunderbird [Windows]", parser.get('thunderbird', 'windows_url'))
+#download_application("Thunderbird [Mac OS]", parser.get('thunderbird', 'mac_url'))
+#download_application("Torbirdy", parser.get('torbirdy', 'url'))
+#download_application("Enigmail", parser.get('enigmail', 'url'))
+#download_application("Vidalia [Linux]", parser.get('vidalia', 'linux_url'))
+#download_application("Vidalia [Windows]", parser.get('vidalia', 'windows_url'))
+#download_application("Vidalia [Mac OS]", parser.get('vidalia', 'mac_url'))
 
 print "[INFO] Extracting Thunderbird [Linux]"
 print "[INFO] Extracting Thunderbird [Windows]"
 print "[INFO] Extracting Thunderbird [Mac OS]"
 print "[INFO] Configure Extensions and Profile Folder"
 
-
 print "[INFO] Unmounting Truecrypt Container"
+
+tc = parser.get('truecrypting', 'unmount')
+tc_unmount = shlex.split(tc)
+subprocess.check_call(tc_unmount)
+
 print "[INFO] Unmounting USB-Stick"
 
 try:
@@ -129,3 +155,4 @@ print "[INFO] Cleaning up Temporary Directories"
 # Removed for debugging purposes, should be reenabled in release:
 #os.removedirs(mountpoint)
 #os.removedirs(tempdir)
+#os.removedirs(tc_mountpoint)
