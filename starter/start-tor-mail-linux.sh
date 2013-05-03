107fd75d544bd84153e24c00eff8a3e89ceda914
Jakob Hasselmann <jafeha@box.(none)>
2013-05-03 21:38:48 +0200

    Updated for gpg

--- starter/start-tor-mail-linux.sh
+++ starter/start-tor-mail-linux.sh
@@ -238,6 +238,7 @@ else
 fi
 
 export GNUPGHOME=$PWD/data/gpg/
+echo user_pref\(\"extensions.enigmail.agentPath\", \"$PWD/apps/linux/gpg4usb/bin/gpg\"\)\; > $PWD"/conf/user-linux.js"
 cp "$PWD/conf/user-linux.js" "$PWD/data/profile/users.js"
 "$PWD/apps/linux/thunderbird/thunderbird/thunderbird" --no-remote -profile "$PWD/data/profile/"
 
